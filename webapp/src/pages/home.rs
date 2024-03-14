13140267abffe91a4c66b252ff9019315cb8b48b
Erik Gassler <erik@gassler.us>
2024-03-14 13:55:49 -0500

    WebUI Update

--- webapp/src/pages/home.rs
+++ webapp/src/pages/home.rs
@@ -1,12 +1,12 @@
 use crate::prelude::*;
 
-pub(crate) fn page_index(contexts: Contexts) -> Html {
+pub(crate) fn page_index(contexts: &Contexts) -> Html {
     push_state("/software-engineer-standards-and-practices");
     page_home(contexts)
 }
 
 /// App home page
-pub(crate) fn page_home(_contexts: Contexts) -> Html {
+pub(crate) fn page_home(_contexts: &Contexts) -> Html {
     set_title(
         "Software Engineer Standards & Practices for Agile Development and Continuous Delivery",
     );
